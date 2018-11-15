2c4c062ce338bafbedce1502446f230177d84dba
Rafael Acosta <rafaco@gmail.com>
2018-11-15 18:29:39 +0100

    CustomRunnableList multiplication fixed

--- app/src/main/java/es/rafaco/devtoollib/SampleApp.java
+++ app/src/main/java/es/rafaco/devtoollib/SampleApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import es.rafaco.devtools.DevTools;
 import es.rafaco.devtools.DevToolsConfig;
+import es.rafaco.devtools.logic.integrations.RunnableConfig;
 
 
 public class SampleApp extends Application {
@@ -13,9 +14,21 @@ public class SampleApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        //TODO: we have currently a double install without and with configuration
         DevTools.install(this, DevToolsConfig.newBuilder()
             .addEmail("rafaco@gmail.com")
             .build()
         );
+
+        //DevTools.breakpoint(this);
+
+        DevTools.addCustomRunnable(new RunnableConfig("Show message",
+                R.drawable.ic_run_white_24dp,
+                () -> DevTools.showMessage("Mostrando mensaje"),
+                () -> DevTools.showMessage("Mensaje mostrado")));
+
+        DevTools.addCustomRunnable(new RunnableConfig("Select API...",
+                R.drawable.ic_settings_white_24dp,
+                () -> DevTools.showMessage("Not already implemented")));
     }
 }
--- app/src/main/java/es/rafaco/devtoollib/view/MainActivity.java
+++ app/src/main/java/es/rafaco/devtoollib/view/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,7 +17,6 @@ import es.rafaco.devtoollib.SampleApp;
 import es.rafaco.devtoollib.api.Controller;
 import es.rafaco.devtools.DevTools;
 import es.rafaco.devtools.logic.steps.FriendlyLog;
-import es.rafaco.devtools.logic.integrations.RunnableConfig;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,62 +25,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FriendlyLog.log("I", "User", "Touch", "User clicked on FloatingActionButton");
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                throw new NullPointerException("A simulated exception from MainActivity fab button");
-                /*Intent intent = new Intent(getApplicationContext(), CrashActivity.class);
-                intent.putExtra("TITLE", "title");
-                intent.putExtra("MESSAGE", "exMessage");
-                getApplicationContext().startActivity(intent);*/
-            }
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            FriendlyLog.log("I", "User", "Touch", "User clicked on FloatingActionButton");
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            throw new NullPointerException("A simulated exception from MainActivity fab button");
         });
 
         AppCompatButton showTools = findViewById(R.id.show_tools);
-        showTools.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FriendlyLog.log("I", "User", "Touch", "User clicked on ShowDevTools");
-                DevTools.openTools(false);
-            }
+        showTools.setOnClickListener(v -> {
+            FriendlyLog.log("I", "User", "Touch", "User clicked on ShowDevTools");
+            DevTools.openTools(false);
         });
 
         AppCompatButton browseDemo = findViewById(R.id.browse);
-        browseDemo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FriendlyLog.log("I", "User", "Touch", "User clicked on Browse Demo");
-                Intent intent = new Intent(getApplicationContext(), ItemListActivity.class);
-                startActivity(intent);
-            }
+        browseDemo.setOnClickListener(v -> {
+            FriendlyLog.log("I", "User", "Touch", "User clicked on Browse Demo");
+            Intent intent = new Intent(getApplicationContext(), ItemListActivity.class);
+            startActivity(intent);
         });
 
         Controller controller = new Controller();
         controller.start(getApplicationContext());
 
         Log.d(SampleApp.TAG, "MainActivity onCreate() performed");
-        //DevTools.breakpoint(this);
-
-        DevTools.addCustomRunnable(new RunnableConfig("Show message",
-                R.drawable.ic_run_white_24dp,
-                () -> DevTools.showMessage("Mostrando mensaje"),
-                () -> DevTools.showMessage("Mensaje mostrado")));
-
-        DevTools.addCustomRunnable(new RunnableConfig("Select API...",
-                R.drawable.ic_settings_white_24dp,
-                () -> DevTools.showMessage("Not already implemented")));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
 
@@ -90,12 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
