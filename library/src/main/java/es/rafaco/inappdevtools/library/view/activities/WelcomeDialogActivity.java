2cdd430c7b2215ae662f6881a513f50c2b54f801
Rafaco <rafaco@gmail.com>
2019-01-15 00:25:18 +0100

    Fixed a lint error and some changes to welcome dialog

--- library/src/main/java/es/rafaco/inappdevtools/library/view/activities/WelcomeDialogActivity.java
+++ library/src/main/java/es/rafaco/inappdevtools/library/view/activities/WelcomeDialogActivity.java
@@ -24,18 +24,20 @@ public class WelcomeDialogActivity extends AppCompatActivity {
     }
 
     private void buildDialog() {
-
         InfoHelper helper = new InfoHelper();
         String elapsedTimeLowered = DateUtils.getElapsedTimeLowered(helper.getAppBuildTime(getApplicationContext()));
+        String message = helper.getFormattedAppName() + "\n" + "Build "  + elapsedTimeLowered + "\n\n" +
+                        "This app contains a set of tool to log, report and inspect.\n\n";
+
+
         ContextWrapper ctw = new ContextThemeWrapper(this, R.style.LibTheme_Dialog);
         final AlertDialog.Builder builder = new AlertDialog.Builder(ctw);
         LayoutInflater inflater = getLayoutInflater();
         //View dialogView = inflater.inflate(R.layout.dialog_welcome, null);
         //builder.setView(dialogView)
-        builder.setTitle("Test version")
-                .setMessage(helper.getFormattedAppName() + "\n" + "Build "  + elapsedTimeLowered + "\n\n" +
-                        "This app contains a tool to inspect your running app. It's also auto-logging what is happening underneath and it allow you to report straight to the developer team.\n\nYou can send them bugs, screenshot, logs, reproduction steps or just your feedback. To start reporting you can:\n - Shake your device\n - Use the notification\n - Slide up from bottom of screen")
+        builder.setTitle("DevTools")
                 .setIcon(R.drawable.ic_warning_yellow_24dp)
+                .setMessage(message)
                 .setPositiveButton("CONTINUE", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -43,11 +45,11 @@ public class WelcomeDialogActivity extends AppCompatActivity {
                         finish();
                     }
                 })
-                .setNegativeButton("OPEN TOOLS", new DialogInterface.OnClickListener() {
+                .setNegativeButton("DISABLE", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         alertDialog.dismiss();
-                        DevTools.openTools(true);
+                        DevTools.showMessage("Disabled coming soon");
                         finish();
                     }
                 })
