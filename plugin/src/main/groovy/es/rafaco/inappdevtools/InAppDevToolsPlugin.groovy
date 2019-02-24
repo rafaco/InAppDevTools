f26dcc482d2d3b91b3e0bd39b9493d18b8c4ebc3
Rafaco <rafaco@gmail.com>
2019-02-24 21:28:46 +0100

    generateGitConfig moved into plugin

--- plugin/src/main/groovy/es/rafaco/inappdevtools/InAppDevToolsPlugin.groovy
+++ plugin/src/main/groovy/es/rafaco/inappdevtools/InAppDevToolsPlugin.groovy
@@ -14,40 +14,27 @@ import groovy.json.JsonOutput
 class InAppDevToolsPlugin implements Plugin<Project> {
 
     final TAG = 'inappdevtools'
+    def project
+    def extension
+    def outputPath
+    def outputFolder
 
     void apply(Project project) {
         def startTime
+        this.project = project
+        this.extension = project.extensions.create(TAG, InAppDevToolsExtension)
+        this.outputPath = "${project.buildDir}/generated/assets/inappdevtools"
+        this.outputFolder = project.file(outputPath)
 
-        def extension = project.extensions.create(TAG, InAppDevToolsExtension)
-
-        def outputPath = "${project.buildDir}/generated/assets/inappdevtools"
-        def outputFolder = project.file(outputPath)
         outputFolder.parentFile.mkdirs()
         project.android.sourceSets.main.assets.srcDirs += "${project.buildDir}/generated/assets"
 
         project.afterEvaluate({
-            Map configMap = [
-                    BUILD_TIME:  new Date().getTime(),
-                    BUILD_TIME_UTC:  new Date().format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC")),
-                    EXT_EMAIL : extension.email,
-                    EXT_ENABLED : extension.enabled,
-                    EXT_DEBUG : extension.debug
-            ]
-
-            //def gitSha = 'git rev-parse --short HEAD'.execute([], project.rootDir).text.trim()
-            //project.android.defaultConfig.buildConfigField "String", "GIT_SHA", "\"${gitSha}\""
-            //println "GIT_SHA" + " = " + "\"${gitSha}\""
-
-            File file = getFile(project, "${outputPath}/compile_config.json")
-            String extensionJson
-            if (!extension.debug) {
-                extensionJson = JsonOutput.toJson(configMap)
-            }else{
-                extensionJson = JsonOutput.prettyPrint(JsonOutput.toJson(configMap))
-                println "CompileConfig: " + file.getPath()
-                println extensionJson
-            }
-            file.write extensionJson
+            def configStartTime = Instant.now()
+            generateCompileConfig(project)
+            generateGitConfig(project)
+            def duration = Duration.between(configStartTime, Instant.now()).toSeconds()
+            println "   InAppDevToolsPlugin:generateConfigs for ${project.name} took " + duration + " seconds"
         })
 
         project.task('onStart',
@@ -120,7 +107,7 @@ class InAppDevToolsPlugin implements Plugin<Project> {
             def counter = 0
             eachFile {
                 counter++
-                if (extension.debug){  println it.path }
+                if (extension.debug){ println it.path }
             }
             doLast{
                 println "Copied ${counter} files into ${outputName}"
@@ -140,14 +127,13 @@ class InAppDevToolsPlugin implements Plugin<Project> {
 
         project.tasks.whenTaskAdded { theTask ->
             if (theTask.name.contains("generate") & theTask.name.contains("ResValues")) {
-                println "InAppDevTools: Added task run before " + theTask.name
+                if (extension.debug){ println "InAppDevTools: Added task run before " + theTask.name }
                 theTask.dependsOn project.tasks.run
             }
         }
 
         project.tasks.clean {
-            delete "src/main/res/raw/${project.name}_sources.jar"
-            delete "src/main/res/raw/${project.name}_resources.zip"
+            delete outputPath
         }
     }
 
@@ -155,5 +141,84 @@ class InAppDevToolsPlugin implements Plugin<Project> {
         def file = project.file(path)
         file.parentFile.mkdirs()
         file
+    }
+
+    private void generateCompileConfig(Project project) {
+        Map propertiesMap = [
+                BUILD_TIME:  new Date().getTime(),
+                BUILD_TIME_UTC:  new Date().format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC")),
+                EXT_EMAIL : extension.email,
+                EXT_ENABLED : extension.enabled,
+                EXT_DEBUG : extension.debug
+        ]
+
+        File file = getFile(project, "${outputPath}/compile_config.json")
+        saveConfigMap(propertiesMap, file)
+    }
+
+    private void generateGitConfig(Project project) {
+
+        Map propertiesMap
+        def gitDiff = shell('git diff HEAD')
+
+        if (gitDiff == null){
+            println TAG + ": " + "Unable to reach git command, check your PATH!"
+            propertiesMap = [
+                    ENABLED:  false
+            ]
+        }else{
+            propertiesMap = [
+                    ENABLED:  true,
+                    INFO: shell('git describe --tags --always --dirty'),
+                    BRANCH: (shell('git branch') =~ /(?m)\* (.*)$/)[0][1],
+                    SHA: shell('git rev-parse --short HEAD'),
+                    TAG : shell('git describe --tags --abbrev=0'),
+                    LAST_COMMIT : [
+                            ISCLEAN: gitDiff == '',
+                            MESSAGE: shell('git log -1 --pretty=%B'),
+                            SHORT: shell('git log --oneline -1'),
+                            LONG: shell('git log -1')
+                    ],
+                    LOCAL_CHANGES: [
+                            ISDIRTY: gitDiff != '',
+                            STATUS: shell('git status --short'),
+                    ]
+            ]
+        }
+
+        File file = getFile(project, "${outputPath}/git_config.json")
+        saveConfigMap(propertiesMap, file)
+
+        if (gitDiff != null && gitDiff != ''){
+            new File("${outputPath}/git.diff").text = gitDiff
+        }
+    }
+
+    private void saveConfigMap(Map map, File file) {
+        String extensionJson
+        if (!extension.debug) {
+            extensionJson = JsonOutput.toJson(map)
+        } else {
+            extensionJson = JsonOutput.prettyPrint(JsonOutput.toJson(map))
+            println "Generated config: " + file.getPath()
+            println extensionJson
+        }
+        file.write extensionJson
+    }
+
+    private String shell(String cmd) {
+        String result = null
+        try {
+            result = cmd.execute([], project.rootDir).text.trim()
+        }
+        catch (java.io.IOException e) {
+            println TAG + "[WARNING]: " + "Unable to reach git command, check your PATH!"
+            if (extension.debug) { e.printStackTrace() }
+        }
+        catch (Exception e) {
+            println TAG + "[WARNING]: " + "Unable to get git info"
+            if (extension.debug) { e.printStackTrace() }
+        }
+        result
     }
 }
