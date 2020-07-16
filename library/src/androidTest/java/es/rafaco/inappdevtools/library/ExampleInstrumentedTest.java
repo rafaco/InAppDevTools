a6de1c27da77b5dd9d73f744e7508fad18eb265b
Rafael Acosta <rafaco@gmail.com>
2020-07-16 18:47:29 +0200

    Fix for Android Studio 4: BuildConfig.APPLICATION_ID has been deprecated. Applied on ExampleInstrumentedTest

--- library/src/androidTest/java/es/rafaco/inappdevtools/library/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/es/rafaco/inappdevtools/library/ExampleInstrumentedTest.java
@@ -8,6 +8,8 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import es.rafaco.inappdevtools.library.logic.utils.ReflexionUtils;
+
 import static org.junit.Assert.*;
 
 /**
@@ -22,6 +24,21 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("es.rafaco.inappdevtools.library", BuildConfig.APPLICATION_ID);
+        assertEquals("es.rafaco.inappdevtools.library", getLibraryPackageName());
+    }
+
+    private String getLibraryPackageName() {
+        // Starting from Android Studio 4, Gradle 6 or Android Gradle Plugin 4:
+        // APPLICATION_ID has been deprecated for libraries and replaced by LIBRARY_PACKAGE_NAME
+
+        if (ReflexionUtils.doesClassContainField(BuildConfig.class,
+                "LIBRARY_PACKAGE_NAME")) {
+            return ReflexionUtils.getStringFieldFromClass(BuildConfig.class,
+                    "LIBRARY_PACKAGE_NAME");
+        }
+        else {
+            return ReflexionUtils.getStringFieldFromClass(BuildConfig.class,
+                    "APPLICATION_ID");
+        }
     }
 }
